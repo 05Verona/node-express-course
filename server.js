2da91934ff65e84bec8a35ddf41b3ca914ecda4a
05Verona <vinicius.verona@aluno.ufop.edu.br>
2020-12-08 22:04:32 -0300

    add first GET route

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
 const express = require('express'); // access express library in node_modules
 const app = express(); // express constructor
 
+// Creating object to contain user data
+const mockUserData = [
+        { name: 'Mark' },
+        { name: 'Jill' }
+]
+
+// Adding first GET route to system
+app.get('/users', (req,res) => {
+        res.json({
+                sucess: true,
+                message: "successfully got users. Nice!",
+                users: mockUserData
+        })
+})
+
+
+/**
+ * ----- Server ------
+ * You can reset the server automatically when you make changes using a tool called 
+ */
 app.listen(8000, () => console.log("server is running")) // start server in port 8000
